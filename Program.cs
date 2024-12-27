4e468f718b4a3d587164ee3a53382a4bb17996d8
sreyas <sreyas1045@gmail.com>
2024-12-27 12:50:34 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.NetworkInformation;
+using System.Text;
+
+Ping pingSender = new Ping();
+PingOptions pingOptions = new PingOptions();
+pingOptions.DontFragment = true;
+
+string data = "Learning to code";
+
+byte[] buffer = Encoding.ASCII.GetBytes(data);
+int timeout = 120;
+string address = "4.2.2.2";
+PingReply reply = pingSender.Send(address , timeout ,buffer ,pingOptions);
+if(reply.Status == IPStatus.Success)
+{
+    Console.WriteLine($"Response : {reply.Status.ToString()}");
+    Console.WriteLine($"RoundTrip : {reply.RoundtripTime}");
+}   
